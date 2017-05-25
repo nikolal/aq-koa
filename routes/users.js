fb6f10e502b0f09804c80a4840a05a47617fac0c
Nikola Lukic <iamnikolalukic@gmail.com>
2017-05-25 15:35:00 +0200

    Deployed to heroku

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
-var router = require('koa-router')();
+const router = require('koa-router')();
 
 router.prefix('/users');
 
 router.get('/', function *(next) {
-  this.body = 'this is a users response!adasd';
+  this.body = 'this is a users response!';
 });
 
 router.get('/bar', function *(next) {
-  console.log(this.params);  // here is always be []
-console.log(this.request.params); // here is always be 'undefined'
   this.body = { name: 'testUser' };
 });
 
